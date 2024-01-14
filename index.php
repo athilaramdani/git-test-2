ce77eb95912abbf35eb2c9b3e03d8704462ad8e5
athilaramdani <athilaramdani@gmail.com>
2024-01-14 16:57:47 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>hello</title>
 </head>
+
 <body>
     <p>hello world</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis recusandae molestiae quia dolores reprehenderit, optio ea totam assumenda, nam ipsum libero, distinctio at consectetur fugiat suscipit necessitatibus eius perspiciatis blanditiis!</p>
+    <ul>
+        <li>gatau wkwkwk</li>
+        <li>gatau wkwkwk</li>
+    </ul>
 </body>
+
 </html>
